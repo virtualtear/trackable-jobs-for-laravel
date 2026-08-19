41e33c537ea418f6a6e8a50bfbccf013473d7bb7
virtualtear <56792071+virtualtear@users.noreply.github.com>
2026-08-19 09:36:05 +0200

    Update migration file publishing timestamps

--- src/Providers/TrackableJobsServiceProvider.php
+++ src/Providers/TrackableJobsServiceProvider.php
@@ -17,8 +17,11 @@ class TrackableJobsServiceProvider extends ServiceProvider
             __DIR__.'/../../config/trackable-jobs.php' => config_path('trackable-jobs.php'),
         ], 'trackable-jobs-assets');
 
+        $publishedAt = time();
+
         $this->publishes([
-            __DIR__.'/../../database/migrations/laravel_trackable_create_tracked_jobs_table.php' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_tracked_jobs_table.php'),
+            __DIR__.'/../../database/migrations/laravel_trackable_create_tracked_jobs_table.php' => database_path('migrations/'.date('Y_m_d_His', $publishedAt).'_create_tracked_jobs_table.php'),
+            __DIR__.'/../../database/migrations/add_queue_column_to_tracked_jobs_table.php' => database_path('migrations/'.date('Y_m_d_His', $publishedAt + 1).'_add_queue_column_to_tracked_jobs_table.php'),
         ], 'trackable-jobs-assets');
 
         Event::listen(JobQueued::class, UpdateTrackedJobStatus::class);
